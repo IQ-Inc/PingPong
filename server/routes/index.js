d003098aec9e34065fd8ba67a4c2885af28f3f1d
Aadu Pirn <aap75@pitt.edu>
2018-02-13 15:39:25 -0500

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ const playersController = require('../controllers').players;
 
 module.exports = (app) => {
   app.get('/api', (req, res) => res.status(200).send({
-    message: 'Welcome to the Ping Pong API!',
+    message: 'Welcome to the Ping Pong API!!!!',
   }));
 
   app.post('/api/players', playersController.create);
