7a5c03005e43a678b12c82b8cfdea0c72838fb83
Aadu Pirn <pirnaadu@gmail.com>
2018-02-14 23:18:39 -0500

    deploying to github pages

--- ping-pong-client/src/static/constants.js
+++ ping-pong-client/src/static/constants.js
@@ -1,1 +1,2 @@
-export const server_url = "http://localhost:8081/";
+//export const server_url = "http://localhost:8081/";
+export const server_url = "http://ping-pong-env.qtiruet3fh.us-east-2.elasticbeanstalk.com/";
